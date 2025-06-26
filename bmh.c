48b5cc9b9024557e30aea931910cf9efdcb599e8
Ramon Caique De Paula <132967661+Ramon-UFSJ2023@users.noreply.github.com>
2025-06-26 10:39:41 -0300

    finalizando

--- bmh.c
+++ bmh.c
@@ -35,7 +35,98 @@ int bmhBuscaNaoComprimido(const unsigned char *texto, size_t tamanhoTexto, const
         }else{
             i+= tabDesloc[texto[i+tamanhoPadrao-1]];
         }
-        *tempoExecução = (double)(clock()-IniTempo)/CLOCKS_PER_SEC;
-        return numOco;
     }
+    *tempoExecução = (double)(clock()-IniTempo)/CLOCKS_PER_SEC;
+    return numOco;
+}
+
+int bmhLerBits(const unsigned char *buffer, size_t bitOffSet, size_t numBits, unsigned long long *bitsLidos){
+    if(numBits >sizeof(unsigned long long)*CHAR_BIT || numBits ==0) return -1;
+    *bitsLidos = 0;
+    size_t bytesLidos = 0;
+    size_t bitsRestante = numBits;
+    size_t byteInicial = bitOffSet/CHAR_BIT;
+    int offsetByte = bitOffSet%CHAR_BIT;
+
+    unsigned char primeiroByte = buffer[byteInicial];
+    size_t bitsPrimByte = (CHAR_BIT - offsetByte >numBits)? numBits : (CHAR_BIT-offsetByte);
+    unsigned char mascara = ((1 << bitsPrimByte) -1);
+    mascara <<= (CHAR_BIT -offsetByte -bitsPrimByte);
+    *bitsLidos = (primeiroByte & mascara) >> (CHAR_BIT-offsetByte-bitsPrimByte);
+    bitsRestante -= bitsPrimByte;
+
+    if(bitsRestante>0){
+        byteInicial++;
+        unsigned char proxByte = buffer[byteInicial];
+        *bitsLidos = (*bitsLidos << bitsRestante) | (proxByte >> (CHAR_BIT -bitsRestante));
+    }
+    return 0;
+}
+
+int bmhBuscaComprimido(const unsigned char *textoComprimido, size_t tamanhoTextoComprimido, const unsigned *padraoComprimido, size_t tamanhoPadraoCmp, long long *numCmp, double *tempExecucao, size_t *ocorrenciasEncontradas, size_t maximoOcorrencias, const NoHuff *raizHuff){
+    clock_t iniTempo = clock();
+    *numCmp = 0;
+    int numOco = 0;
+    size_t ocorrenInd=0;
+
+    if(tamanhoPadraoCmp == 0 || tamanhoPadraoCmp >tamanhoTextoComprimido){
+        *tempExecucao = (double)(clock()-iniTempo)/ CLOCKS_PER_SEC;
+        return 0;
+    }
+    size_t i=0;
+    while(i <= tamanhoTextoComprimido-tamanhoPadraoCmp){
+        bool match= false;
+        for(size_t j=0; j<tamanhoPadraoCmp;j++){
+            (*numCmp)++;
+            unsigned long long bitPadrao, bitTexto;
+            if(bmhLerBits(padraoComprimido,j,1,&bitPadrao) != 0){
+                fprintf(stderr,"Erro de leitura do bit do padrao comprimido.\n");
+                *tempExecucao =(double)(clock()-iniTempo)/CLOCKS_PER_SEC;
+                return -1;
+            }
+            if(bmhLerBits(textoComprimido,i+j,1,&bitTexto) != 0){
+                match = true;
+                break;
+            }
+            if(bitPadrao != bitTexto){
+                match = true;
+                break;
+            }
+        }
+        if(!match){
+            size_t posiOriginalCaractere = 0;
+            size_t bitsPercorridos = 0;
+            const NoHuff *tempNo = raizHuff;
+
+            size_t bitAtualPos = 0;
+            size_t caracDecodCont=0;
+            if(raizHuff != NULL){
+                tempNo = raizHuff;
+                while(bitAtualPos<i){
+                    unsigned long long bitAtualVal;
+                    if(bmhLerBits(textoComprimido,bitAtualPos,1,&bitAtualVal)!=0) break;
+
+                    if(bitAtualVal == 0){
+                        tempNo = tempNo->esquerda;
+                    }else{
+                        tempNo = tempNo->direita;
+                    }
+                    bitAtualPos++;
+                    if(tempNo->esquerda == NULL && tempNo->direita == NULL){
+                        caracDecodCont++;
+                        tempNo = raizHuff;
+                    }
+                }
+                posiOriginalCaractere = caracDecodCont;
+            }
+            if(ocorrenInd <maximoOcorrencias) ocorrenciasEncontradas[ocorrenInd++] = posiOriginalCaractere;
+            numOco++;
+            i += tamanhoPadraoCmp;
+        }else{
+            i++;
+
+        }
+    }
+    *tempExecucao = (double)(clock()- iniTempo)/CLOCKS_PER_SEC;
+    return numOco;
 }
